066d8edb97d37f61eda52f9e7fb863a7267b4f06
josuex09 <josuex09@hotmail.es>
2015-09-02 14:26:14 -0600

    archivos iniciales

---
+++ cola.h
@@ -0,0 +1,8 @@
+//
+// Created by josue on 9/2/15.
+//
+
+#ifndef SERVIDORHTTP_COLA_H
+#define SERVIDORHTTP_COLA_H
+
+#endif //SERVIDORHTTP_COLA_H
--- main.c
+++ main.c
@@ -1,7 +1,35 @@
+#include "stdlib.h"
 #include <stdio.h>
+#include "unistd.h"
+int main(int argc, char ** argv){
+    //arg1 puerto
+    //arg2 direccion de la carpeta wwww
+    //arg3 numero del modo
+    // si arg3 = 4 o 5 , arg4 k procesos que van a correr
+
+    int modo = atoi(argv[3]);
+
+    if((modo == 4 || modo ==5) && argc != 5){
+        printf("\nLa forma correcta de correrlo es: ServidorHTTP puerto ruta modo\nSi el modo es 3(Pre-forked) o 4(Pre-threaded) se agrega un cuarto de los k procesos que van a correr \nEjemplo: ServidorHTTP 8081 temp/wwwroot 2\n");
+        exit(0);
+    }
+    else if(modo <0 || modo > 3 || argc !=4){
+        printf("\nLa forma correcta de correrlo es: ServidorHTTP puerto ruta modo\nSi el modo es 3(Pre-forked) o 4(Pre-threaded) se agrega un cuarto de los k procesos que van a correr \nEjemplo: ServidorHTTP 8081 temp/wwwroot 2\n");
+        exit(0);
+    }
+//    int puerto = atoi(argv[1]);
+    if(chdir(argv[2]) == -1){
+        printf("\nError con la ruta %s verifiquela y vuelva a intentarlo\n",argv[2]);
+    }
+
+    if(modo == 1){ //FIFO
+
+    }
 
 
-int main(){
-    printf("%d",1);
+
+
+
+
     return 0;
 }
